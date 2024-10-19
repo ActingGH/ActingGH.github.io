94327bc0d2142cbaa187bcabc359d316d5d958b4
Acting <2864094036@qq.com>
2024-10-19 11:46:08 +0800

    Site updated: 2024-10-19 11:46:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/30/Docker学习/","2023/12/20/MybatisPlus/","2023/12/30/Linux学习/","2023/12/30/MySql高级/","2023/12/06/MySQL/","2023/12/15/SpringMVC学习/","2023/12/30/git学习/","2023/12/30/网络编程/","2023/12/30/微服务架构/","2024/09/26/微信小程序开发部署上线/"];function toRandomPost(){
+var posts=["2023/12/30/Docker学习/","2023/12/30/Linux学习/","2023/12/30/MySql高级/","2023/12/20/MybatisPlus/","2023/12/06/MySQL/","2023/12/15/SpringMVC学习/","2023/12/30/git学习/","2024/09/26/微信小程序开发部署上线/","2023/12/30/微服务架构/","2023/12/30/网络编程/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
